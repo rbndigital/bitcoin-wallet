4d399332dca76c243bd419508b96e96e9e9c318b
Andreas Schildbach <andreas.schildbach@bloq.com>
2018-04-13 11:44:23 +0200

    ScanActivity: Make sure window background is opaque when there is no scene transition animation going to happen.

--- wallet/src/de/schildbach/wallet/ui/scan/ScanActivity.java
+++ wallet/src/de/schildbach/wallet/ui/scan/ScanActivity.java
@@ -55,6 +55,7 @@ import android.content.pm.PackageManager;
 import android.graphics.Rect;
 import android.graphics.RectF;
 import android.graphics.SurfaceTexture;
+import android.graphics.drawable.ColorDrawable;
 import android.hardware.Camera;
 import android.hardware.Camera.CameraInfo;
 import android.hardware.Camera.PreviewCallback;
@@ -174,6 +175,8 @@ public final class ScanActivity extends AbstractWalletActivity
                 // Using alpha rather than visibility because 'invisible' will cause the surface view to never
                 // start up, so the animation will never start.
                 contentView.setAlpha(0);
+                getWindow()
+                        .setBackgroundDrawable(new ColorDrawable(getResources().getColor(android.R.color.transparent)));
                 OnFirstPreDraw.listen(contentView, new OnFirstPreDraw.Callback() {
                     @Override
                     public boolean onFirstPreDraw() {
